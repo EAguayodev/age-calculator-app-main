bed40d1d66cc84cd5f7f6853a3635ed892c6918d
Eric Aguayo <computergeek90@protonmail.com>
2024-10-31 15:25:10 -0500

    testing mobile layout before moving on to starting Javascript

---
+++ age-calculator-app-main/js/main.js
@@ -0,0 +1,6 @@
+//Tasks: invalid errors, empty errors, empty fields, empty fields errors
+// task-2: display results after cta is pushed
+// task-3: display results in the results section
+// task-4: style error states
+// task-5: refactor code
+// task-6: add validations
